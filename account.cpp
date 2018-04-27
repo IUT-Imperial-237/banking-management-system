bb72549840f0cd2697ae5fc3a645dcd63f2345fe
amadou-olabi <36259246+amadou-olabi@users.noreply.github.com>
2018-04-27 17:40:39 +0600

    new updates
    I implemented the copy constructor and overloaded operations for both account and person classes;
    
    Co-Authored-By: iut-cse-4301 <iut-cse-4301@users.noreply.github.com>

--- account.cpp
+++ account.cpp
@@ -3,12 +3,12 @@
 account::account()
 {
     //ctor
-    account_num = "XXX-XXXX-XXX"; //!< Member variable "account_num"
-    account_card = "XXX-XXXX"; //!< Member variable "account_card"
-    account_pin = "XXXX"; //!< Member variable "account_pin"
-    balance = 1000.00; //!< Member variable "balance"
-    status = false; //!< Member variable "status"
-    t_amount = 0.00; //!< Member variable "t_amount"
+    this -> account_num = "XXX-XXXX-XXX"; //!< Member variable "account_num"
+    this -> account_card = "XXX-XXXX"; //!< Member variable "account_card"
+    this -> account_pin = "XXXX"; //!< Member variable "account_pin"
+    this -> balance = 1000.00; //!< Member variable "balance"
+    this -> status = false; //!< Member variable "status"
+    this -> t_amount = 0.00; //!< Member variable "t_amount"
 }
 
 account::~account()
@@ -19,12 +19,24 @@ account::~account()
 account::account(const account& other)
 {
     //copy ctor
+    this -> account_num = other.Getaccount_num();
+    this -> account_card = other.Getaccount_card();
+    this -> account_pin = other.Getaccount_pin();
+    this -> balance = other.Getbalance();
+    this -> status = other.Getstatus();
+    this -> t_amount = other.Gett_amount();
 }
 
 account& account::operator=(const account& rhs)
 {
     if (this == &rhs) return *this; // handle self assignment
     //assignment operator
+    this -> account_num = rhs.Getaccount_num();
+    this -> account_card = rhs.Getaccount_card();
+    this -> account_pin = rhs.Getaccount_pin();
+    this -> balance = rhs.Getbalance();
+    this -> status = rhs.Getstatus();
+    this -> t_amount = rhs.Gett_amount();
     return *this;
 }
 
--- person.cpp
+++ person.cpp
@@ -3,15 +3,15 @@
 person::person()
 {
     //ctor
-    name = "default Name"; //!< Member variable "name"
-    birhtInfos = "default birhtInfos"; //!< Member variable "birthInfos"
-    address = "default address"; //!< Member variable "address"
-    phone = "default phone"; //!< Member variable "phone"
-    country = "default country"; //!< Member variable "country"
-    parentInfo = "default parentInfo"; //!< Member variable "parentInfo"
-    gender = "default gender"; //!< Member variable "gender"
-    email = "default email"; //!< Member variable "email"
-    profession = "default profession"; //!< Member variable "profession"
+    this -> name = "default Name"; //!< Member variable "name"
+    this -> birhtInfos = "default birhtInfos"; //!< Member variable "birthInfos"
+    this -> address = "default address"; //!< Member variable "address"
+    this -> phone = "default phone"; //!< Member variable "phone"
+    this -> country = "default country"; //!< Member variable "country"
+    this -> parentInfo = "default parentInfo"; //!< Member variable "parentInfo"
+    this -> gender = "default gender"; //!< Member variable "gender"
+    this -> email = "default email"; //!< Member variable "email"
+    this -> profession = "default profession"; //!< Member variable "profession"
 
 }
 
@@ -23,12 +23,33 @@ person::~person()
 person::person(const person& other)
 {
     //copy ctor
-
+    this->name = other.Getname();
+    this->birhtInfos = other.GetbirhtInfos();
+    this->address = other.Getaddress();
+    this->phone = other.Getphone();
+    this->country = other.Getcountry();
+    this->parentInfo = other.GetparentInfo();
+    this->gender = other.Getgender();
+    this->email = other.Getemail();
+    this->profession = other.Getprofession();
 }
 
 person& person::operator=(const person& rhs)
 {
-    if (this == &rhs) return *this; // handle self assignment
+    if (this == &rhs)
+        return *this; // handle self assignment
     //assignment operator
-    return *this;
+    else
+    {
+        this->name = rhs.Getname();
+        this->birhtInfos = rhs.GetbirhtInfos();
+        this->address = rhs.Getaddress();
+        this->phone = rhs.Getphone();
+        this->country = rhs.Getcountry();
+        this->parentInfo = rhs.GetparentInfo();
+        this->gender = rhs.Getgender();
+        this->email = rhs.Getemail();
+        this->profession = rhs.Getprofession();
+        return *this;
+    }
 }
